46b273d7344dee8ffc95efc5e5706429b1a56bd1
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-01 20:55:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -54,7 +54,7 @@ end
 def sum_array(array)
   new_array = []
   new_array = array.collect do |element|
-    element.class = "integer"
+    element.class = fixnum
   end
   array.inject(0) {|result, element| result + element}
 end
