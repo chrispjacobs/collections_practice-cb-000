709fb3da61ae5982840a218b1d8e7dc0a6f1fbf3
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-01 20:13:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,4 +8,16 @@ def sort_array_asc(array)
   array_2 = array.sort
 end
 
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.count == b.count
+      0
+    elsif a.count > b.count
+      1
+    elsif a.count < b.count
+      -1
+    end
+  end
+end
+  
 puts sort_array_asc([3, 5, 8, 1, 2])
