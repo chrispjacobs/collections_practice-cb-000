62584d44ce54b24a1715c8da9349d1affe83c339
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-01 21:07:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -62,7 +62,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |element, index|
+  array.each_with_index.each do |element, index|
     unless index == 1
     element = element + "s"
   end
